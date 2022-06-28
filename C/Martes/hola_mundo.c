4793fb0b6697cc8f2024e33ce14a9a2ab781d721
MantiMantilla <alejandromantillaredondo@gmail.com>
2022-06-28 14:14:44 -0500

    Extended .gitignore and added minor C folder files.

---
+++ C/Martes/hola_mundo.c
@@ -0,0 +1,11 @@
+// Ejemplo clásico de Hola Mundo
+#include <stdio.h>
+
+/* Aquí va la función main
+con un comentario previo */
+int main(void) {
+    // Aquí va el código que se ejecuta en la función
+    printf("Hello world");
+    
+    return 0;
+}
